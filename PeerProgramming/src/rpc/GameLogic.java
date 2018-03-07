f46cf92dfd2b4b2759611382153368d72350980b
Amrit Parajuli <amrit.parajuli@outlook.com>
2018-03-07 11:01:52 +0000

    rock paper scissors improved main function.

--- PeerProgramming/src/rpc/GameLogic.java
+++ PeerProgramming/src/rpc/GameLogic.java
@@ -1,6 +1,38 @@
 package rpc;
 
+import java.util.Scanner;
+
 public class GameLogic {
+
+	public static void playGame() {
+		boolean running = true;
+		System.out.println("Rock paper scissors game. Enter rock paper or scissors (case sensitive) "
+				+ "to take a turn, type quit to end");
+		while (running == true) {
+			Scanner q = new Scanner(System.in);
+
+			System.out.println("Please input move:");
+
+			String input = q.nextLine();
+			if (input.equals("quit")) {
+				running = false;
+
+				System.out.println("Game has Ended.");
+				break;
+			}
+
+			String aiInput = GameLogic.aiInput();
+			System.out.println(aiInput);
+
+			String result = GameLogic.findWinner(input, aiInput);
+			System.out.println(result);
+			GameData.inputData(result);
+
+		}
+		System.out.println("Game Data:");
+
+		GameData.displayData();
+	}
 
 	public static String findWinner(String player, String AI) {
 
--- PeerProgramming/src/rpc/RunnerClass.java
+++ PeerProgramming/src/rpc/RunnerClass.java
@@ -1,38 +1,10 @@
 package rpc;
 
-import java.util.Scanner;
-
 public class RunnerClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean running = true;
-		System.out.println("Rock paper scissors game. Enter rock paper or scissors (case sensitive) "
-				+ "to take a turn, type quit to end");
-		while (running == true) {
-			Scanner q = new Scanner(System.in);
 
-			System.out.println("Please input move:");
-
-			String input = q.nextLine();
-			if (input.equals("quit")) {
-				running = false;
-
-				System.out.println("Game has Ended.");
-				break;
-			}
-
-			String aiInput = GameLogic.aiInput();
-			System.out.println(aiInput);
-
-			String result = GameLogic.findWinner(input, aiInput);
-			System.out.println(result);
-			GameData.inputData(result);
-
-		}
-		System.out.println("Game Data:");
-
-		GameData.displayData();
+		GameLogic.playGame();
 	}
-
 }
